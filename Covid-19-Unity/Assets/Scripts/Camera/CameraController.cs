963460763fe5dfd04c7d48f75c21ad00f1450fa0
Marco Ravelo <mravelo5874@gmail.com>
2020-04-12 22:28:14 -0500

    init

---
+++ Covid-19-Unity/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target; // object that camera will follow
+    public Vector3 offset; // camera offset to target
+    public float smoothSpeed = 0.3f;
+    private Vector3 velocity;
+
+    private void LateUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, target.position + offset, ref velocity, smoothSpeed);
+    }
+}
---
+++ Covid-19-Unity/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Bools:
+    private bool isPaused = false;
+    private bool isMove = false;
+
+    // Movement:
+    public float playerSpeed = 5.0f; // player movement speed
+    private Rigidbody2D playerRigidBody; // player rigid-body component
+    private Vector2 playerVector; // keep track of the player's movement vector
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        playerVector = new Vector2(0, 0);
+    }   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isPaused)
+        {
+            // reset player vector
+            playerVector.x = 0;
+            playerVector.y = 0;
+
+            // reset bools
+            isMove = false;
+
+            // get vertical movement
+            if (Input.GetKey(KeyCode.W))
+            {
+                playerVector.y += 1;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                playerVector.y -= 1;
+            }
+            // get horizontal movement
+            if (Input.GetKey(KeyCode.D))
+            {
+                playerVector.x += 1;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                playerVector.x -= 1;
+            }
+
+            if (playerVector.x != 0 || playerVector.y != 0)
+            {
+                isMove = true;
+            }
+            
+        }
+    }
+    
+
+
+    void FixedUpdate() 
+    {
+        if (isMove)
+        {
+            // apply player vector to rigid-body
+            Vector3 movementVector = new Vector3(playerVector.x, playerVector.y, 0);
+            movementVector = movementVector.normalized * playerSpeed * Time.deltaTime;
+            // reduce speed when moving diagonal
+            if (playerVector.x != 0 && playerVector.y != 0)
+            {
+                movementVector /= Mathf.Sqrt(2);
+            }
+
+            playerRigidBody.MovePosition(playerRigidBody.transform.position + movementVector);
+        }
+    }
+}
