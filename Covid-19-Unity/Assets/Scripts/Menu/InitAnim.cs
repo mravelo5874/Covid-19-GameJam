fba4ac554e73561d102d7ef31327fb1c72115279
Marco Ravelo <mravelo5874@gmail.com>
2020-04-19 15:34:03 -0500

    added into

---
+++ Covid-19-Unity/Assets/Scripts/Menu/InitAnim.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class InitAnim : MonoBehaviour
+{
+    public Animator Fade;
+    public Animator CtrlZ;
+
+    void Start()
+    {
+        Fade.Play("Black");
+        CtrlZ.Play("Remove");
+        StartCoroutine(Init());
+    }
+
+    private IEnumerator Init()
+    {
+        yield return new WaitForSeconds(2f);
+        Fade.Play("FadeIn");
+        yield return new WaitForSeconds(1f);
+        Fade.Play("Clear");
+        yield return new WaitForSeconds(0.5f);
+        CtrlZ.Play("ctrlz");
+        yield return new WaitForSeconds(6f);
+        Fade.Play("FadeOut");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("MainMenu");
+    }
+}
